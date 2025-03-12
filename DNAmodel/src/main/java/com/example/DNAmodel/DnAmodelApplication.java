a3ace54c8cc660b7069c6b0c1199d1453f418c68
Ulukbek11 <lucky1hlucky1h@gmail.com>
2025-03-12 17:01:59 +0600

    Add files via upload

---
+++ DNAmodel/src/main/java/com/example/DNAmodel/DnAmodelApplication.java
@@ -0,0 +1,24 @@
+package com.example.DNAmodel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@SpringBootApplication
+public class DnAmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnAmodelApplication.class, args);
+	}
+
+}
+
+
+
---
+++ DNAmodel/src/test/java/com/example/DNAmodel/DnAmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DNAmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnAmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
